08e164d78a7c35ad94c2afc7710e304d50a27f6a
mrtaalebi <the.doors.are.locked@gmail.com>
2019-07-01 23:58:04 +0430

    asghar didi adama doset nadaran

--- apps/intro/urls.py
+++ apps/intro/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from django.urls import path, include
 from apps.intro import views
-from django.conf.urls import i18n as i18n_url
+
+
 app_name = "intro"
 urlpatterns = [
     url(r'$', views.homepage),
-    # url(r'^i18n/', include(i18n_url), name='i18n'),
 ]
--- apps/intro/views.py
+++ apps/intro/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def homepage(request):
     print(request.LANGUAGE_CODE)
     return render(request, 'intro/homepage.html')
+
