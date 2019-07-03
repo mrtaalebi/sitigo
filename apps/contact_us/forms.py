dced036fa6e27a2e8bdb741b4465bcd441c7c741
mrtaalebi <the.doors.are.locked@gmail.com>
2019-07-03 11:29:08 +0430

    ?

--- apps/contact_us/forms.py
+++ apps/contact_us/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-# from captcha.fields import ReCaptchaField
+from captcha.fields import ReCaptchaField
 
 from .models import ContactInfo
 
@@ -8,5 +8,17 @@ class ContactInfoForm(forms.Form):
     class Meta:
         model = ContactInfo
         fields = '__all__'
-        #captcha = ReCaptchaField()
+
+    name = forms.CharField(max_length=50, required=True)
+    email = forms.CharField(max_length=50, required=True)
+    phone_number = forms.CharField(max_length=50, required=True)
+
+    country = forms.CharField(max_length=50, required=True)
+    school_uni = forms.CharField(max_length=50, required=True)
+    study_grade = forms.CharField(max_length=50, required=True)
+
+    subject = forms.CharField(max_length=100, required=True)
+    text = forms.CharField(max_length=500, widget=forms.Textarea, required=True)
+
+#    captcha = ReCaptchaField()
 
