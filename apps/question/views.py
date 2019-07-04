9a55b5289b72ba81b839fe84d4e6059ccd334f36
arghavan99 <a.rezvani99@yahoo.com>
2019-07-05 00:18:20 +0430

    redirect to homepage

--- apps/question/views.py
+++ apps/question/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,6 +8,8 @@ from apps.question.models import Question
 
 
 def question_answer(request):
+    if translation.get_language() == 'en':
+        return HttpResponseRedirect('/intro/')
     questions = list(Question.objects.all())
     context = {'questions': questions}
     return render(request, 'question/question_answer.html', context)
