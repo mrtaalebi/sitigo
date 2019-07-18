88f7ef1d06e43e5064d5315f104e24359ac72641
mrtaalebi <the.doors.are.locked@gmail.com>
2019-07-18 16:45:18 +0430

    scoreborad admin

--- apps/scoreboard/admin.py
+++ apps/scoreboard/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contestant, Country, Ruler, ProblemScore, ContestTier
+
+
+@admin.register(Contestant)
+class ContestantAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ruler)
+class RulerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProblemScore)
+class ProblemScoreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ContestTier)
+class ContestTierAdmin(admin.ModelAdmin):
+    pass
+
