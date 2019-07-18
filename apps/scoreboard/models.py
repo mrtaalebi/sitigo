a5743a2fa9f34b8d2e677ebb1d0d39166dac0e9d
mrtaalebi <the.doors.are.locked@gmail.com>
2019-07-18 16:41:26 +0430

    lil change in model and migrations

--- apps/scoreboard/models.py
+++ apps/scoreboard/models.py
@@ -4,11 +4,11 @@ from apps.content.models import Event
 
 class Contestant(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    country = models.ForeignKey("Country", on_delete=models.CASCADE)
-    ruler = models.ForeignKey("Ruler", on_delete=models.CASCADE)
+    country = models.ForeignKey("Country", null=False, blank=False, on_delete=models.CASCADE)
+    ruler = models.ForeignKey("Ruler", null=False, blank=False, on_delete=models.CASCADE)
     rank = models.IntegerField(null=False, blank=False)
 
-    event_tier = models.ForeignKey("ContestTier", null=False, blank=False)
+    event_tier = models.ForeignKey("ContestTier", null=False, blank=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name + " - " + self.event_tier
@@ -31,7 +31,7 @@ class Ruler(models.Model):
 class ProblemScore(models.Model):
     p_id = models.IntegerField(null=False, blank=False, unique=True)
     score = models.FloatField(null=False, blank=False)
-    contestant = models.ForiegnKey("Contestant", related_name="problem_scores", null=False, blank=False)
+    contestant = models.ForeignKey("Contestant", related_name="problem_scores", null=False, blank=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.p_id + ": " + self.score + " - " + self.contestant
@@ -40,7 +40,7 @@ class ProblemScore(models.Model):
 class ContestTier(models.Model):
     persian_name = models.CharField(max_length=50, null=False, blank=False)
     english_name = models.CharField(max_length=50, null=False, blank=False)
-    event = models.Foreignkey("Event", null=False, blank=False)
+    event = models.ForeignKey(Event, null=False, blank=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name + " - " + self.event
