cf4bb6821ff93d7084aa5a611544041010c0836f
mrtaalebi <the.doors.are.locked@gmail.com>
2019-07-27 02:00:13 +0430

    scoredboard 4

--- apps/scoreboard/views.py
+++ apps/scoreboard/views.py
@@ -18,21 +18,19 @@ def scoreboard(request, event_id=None):
         'events': list(Event.objects.order_by('year')),
         'event': event,
         'contest_tiers': [
-                {
-                    'tr': tier,
-                    'table_header': 
-                        ['Name', 'Country', ] \
-                        + ["P" + str(sc.p_id) for sc in ProblemScore.objects.filter(contestant=Contestant.objects.first(event_tier=tier)).order_by('p_id')] \
-                        + ['Score', 'Rank', 'Ruler'],
-                    'contestants': [
-                            {
-                                'con': con,
-                                'scores': ProblemScore.objects.filter(contestant=con).order_by('p_id'),
-                                'sum_score': sum(list(ProblemScore.objects.filter(contestant=con))),
-                            }
-                        for con in Contestant.objects.filter(event_tier=tier).order_by('rank')],
-                }
-            for tier in ContestTier.objects.filter(event__id=event.id)]
+            {
+                'tr': tier,
+                'table_header': 
+                    ['Name', 'Country', ] \
+                    + ["P" + str(sc.p_id) for sc in ProblemScore.objects.filter(contestant=Contestant.objects.filter(event_tier=tier).first()).order_by('p_id')] \
+                    + ['Score', 'Rank', 'Ruler'],
+                'contestants': [
+                    {
+                        'con': con,
+                        'scores': ProblemScore.objects.filter(contestant=con).order_by('p_id'),
+                        'sum_score': sum(list(ProblemScore.objects.filter(contestant=con))),
+                    } for con in Contestant.objects.filter(event_tier=tier).order_by('rank')],
+            } for tier in ContestTier.objects.filter(event=event)]
     }
 
     return render(request, 'scoreboard/scoreboard.html', context)
