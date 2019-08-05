9d738ba2f83dc7ef2fb0b2ec841d21a7cca8730e
mrtaalebi <the.doors.are.locked@gmail.com>
2019-08-06 00:03:17 +0430

    onhover caption

--- apps/gallery/static/gallery/script.js
+++ apps/gallery/static/gallery/script.js
@@ -6,6 +6,21 @@ $(document).ready(function () {
         $('.modal-caption').html($(this).attr("alt"))
 	});
 	$('.modal').click(function(){
+        $('.modal-image').attr("src", "");
+        $('.modal-image').attr("alt", "");
+        $('.modal-caption').html("");
 		$('.modal').fadeOut();
 	});
+
+    $(".show-caption").hover(
+        function() {
+            var select = "#caption_" + $(this).attr("id");
+            $(select).fadeIn();
+        },
+
+        function() {
+            var select = "#caption_" + $(this).attr("id");
+            $(select).fadeOut();
+        }
+    );
 });
