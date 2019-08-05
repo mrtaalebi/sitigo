a7614add14891275e39a99cf87f7ec9b0f567218
mrtaalebi <the.doors.are.locked@gmail.com>
2019-08-05 19:37:01 +0430

    hmmmmmmmmm

--- apps/gallery/static/gallery/script.js
+++ apps/gallery/static/gallery/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     $(".open-modal").click(function(){
-	    $('.modal').show();
+	    $('.modal').fadeIn();
         $('.modal-image').attr("src", $(this).attr("src"));
         $('.modal-image').attr("alt", $(this).attr("alt"));
         $('.modal-caption').html($(this).attr("alt"))
 	});
-	$('.open-modal').click(function(){
-		$('.modal').hide();
+	$('.modal').click(function(){
+		$('.modal').fadeOut();
 	});
 });
