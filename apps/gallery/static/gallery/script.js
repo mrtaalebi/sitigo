d93cc203d210af55ca6509ad02a7f110eaae00da
mrtaalebi <the.doors.are.locked@gmail.com>
2019-08-05 18:34:15 +0430

    hmm

--- apps/gallery/static/gallery/script.js
+++ apps/gallery/static/gallery/script.js
@@ -1,9 +1,7 @@
-$(function () {
-    "use strict";
-    
-    $("document.querySelector('#topper > div > div.ui.stackable.grid > div.twelve.wide.center.aligned.stretched.column > div > div > div > div > div > img')").click(function () {
+$(window).load(function () {
+    $(".o-my-sweet-image").click(function () {
         var $src = $(this).attr("src");
-        $("#topper > div > div.ui.stackable.grid > div.twelve.wide.center.aligned.stretched.column > div > div.show").fadeIn();
+        $(".show").fadeIn();
         $(".img-show img").attr("src", $src);
     });
     
