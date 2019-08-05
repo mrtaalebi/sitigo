77b8bf36e07f476979f2664e48f6d2e681b3aee9
mrtaalebi <the.doors.are.locked@gmail.com>
2019-08-05 18:53:50 +0430

    hmmmmm

--- apps/gallery/static/gallery/script.js
+++ apps/gallery/static/gallery/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-	$(".trigger_popup_fricc").click(function(){
-	   $('.hover_bkgr_fricc').show();
+    $(".trigger_popup_fricc").click(function(){
+	    $('.hover_bkgr_fricc').show();
+        $('.hover_img_fricc').attr("src", $(this).attr("src"))
 	});
 	$('.hover_bkgr_fricc').click(function(){
 		$('.hover_bkgr_fricc').hide();
