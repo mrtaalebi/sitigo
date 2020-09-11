e58a7de6cdb9dbe4db95a1b366b611307ba6981a
mrtaalebi <the.doors.are.locked@gmail.com>
2020-09-12 01:22:12 +0430

    Add initdb for db

---
+++ deploy/config/db/docker-entrypoint-initdb.d/apply-dump.sh
@@ -0,0 +1,1 @@
+pg_restore -U sitigouser -d sitigodb ./dump
---
+++ deploy/config/db/docker-entrypoint-initdb.d/create-db-user.sql
@@ -0,0 +1,3 @@
+CREATE USER sitigouser WITH ENCRYPTED PASSWORD passopassopassa;
+CREATE DATABASE sitigodb;
+GRANT ALL PRIVILEGES ON DATABASE sitigodb to sitigouser;
