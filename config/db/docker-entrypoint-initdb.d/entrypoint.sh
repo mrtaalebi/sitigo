e5567fd189877054dba6d49e5d48a391531cdffb
mrtaalebi <the.doors.are.locked@gmail.com>
2020-09-13 19:31:27 +0430

    Add fucking double quoetushoeutnoehusaonts

--- config/db/docker-entrypoint-initdb.d/entrypoint.sh
+++ config/db/docker-entrypoint-initdb.d/entrypoint.sh
@@ -1,5 +1,5 @@
 psql << EOSQL
-CREATE USER sitigouser WITH ENCRYPTED PASSWORD passopassopassa;
+CREATE USER sitigouser WITH ENCRYPTED PASSWORD "passopassopassa";
 CREATE DATABASE sitigodb;
 GRANT ALL PRIVILEGES ON DATABASE sitigodb to sitigouser;
 EOSQL
